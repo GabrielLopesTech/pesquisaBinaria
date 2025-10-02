6cde32992f1f8d02a982a5a2a5d3c002a9bab2ff
Gabriel Lopes dos Santos <gabriel.lsantostech@gmail.com>
2025-10-02 19:44:41 -0300

    tmj professor

--- PesquisaBinaria.py
+++ PesquisaBinaria.py
@@ -43,3 +43,42 @@ print(pesquisa_binaria(numeros, 2))   # Saída: -1 (não existe na lista)
 
 # print(pesquisa_binaria(numeros, 7))   # Saída: 3 (índice do número 7)
 # print(pesquisa_binaria(numeros, 2))   # Saída: -1 (não existe na lista)
+
+# ordenacaoPorSelecao
+
+def encontra_menor_indice(arr):
+    menor = arr[0]
+    menor_indice = 0
+    for i in range(1, len(arr)):
+        if arr[i] < menor:
+            menor = arr[i]
+            menor_indice = i
+    return menor_indice
+
+def ordenacao_por_selecao(arr):
+    novo_arr = []
+    for i in range(len(arr)):
+        menor = encontra_menor_indice(arr)
+        novo_arr.append(arr.pop(menor))
+    return novo_arr
+
+# Exemplo
+print(ordenacao_por_selecao([5, 3, 6, 2, 10]))  
+# saída: [2, 3, 5, 6, 10]
+
+
+# Encontrar o maior número em uma lista
+
+def maior_numero(lista):
+    maior = lista[0]
+    for num in lista:
+        if num > maior:
+            maior = num
+    return maior
+
+# Exemplo
+print(maior_numero([3, 7, 2, 9, 5]))  # saída: 9
+
+
+
+
